bdfe688b059620963c45e27504c6b948a003e5be
Nicolly <nicollylemosdasilva@gmail.com>
2024-06-27 16:35:21 -0300

    Adicionar arquivos de projeto.

---
+++ VacinePet/Controllers/AnimalsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VacinePet.Models;
+
+namespace VacinePet.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        private rafadudanicoEntities db = new rafadudanicoEntities();
+
+        // GET: Animals
+        public ActionResult Index()
+        {
+            return View(db.Animal.ToList());
+        }
+
+        // GET: Animals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animal animal = db.Animal.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animal);
+        }
+
+        // GET: Animals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animals/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Especie,Raca,Sexo,Idade,Peso")] Animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Animal.Add(animal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(animal);
+        }
+
+        // GET: Animals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animal animal = db.Animal.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animal);
+        }
+
+        // POST: Animals/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Especie,Raca,Sexo,Idade,Peso")] Animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(animal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(animal);
+        }
+
+        // GET: Animals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animal animal = db.Animal.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animal);
+        }
+
+        // POST: Animals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Animal animal = db.Animal.Find(id);
+            db.Animal.Remove(animal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
